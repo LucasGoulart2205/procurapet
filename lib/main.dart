4dc259a49eb02c37218f320b5e95546a046de1ea
Lucas <lucasgoulart7878@gmail.com>
2025-11-18 16:08:59 -0300

    atualizando codigo

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env"); // Carrega as variáveis do .env
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
